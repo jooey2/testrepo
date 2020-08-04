6a6088f46b28bdf21eef857d847b782ed36f9473
James Rees <6393840+Jxr227@users.noreply.github.com>
2020-08-04 14:56:56 +0100

    Add a couple of controller tests

--- build.sbt
+++ build.sbt
@@ -10,7 +10,7 @@ scalaVersion := "2.13.3"
 libraryDependencies += guice
 libraryDependencies +=ws
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
-
+libraryDependencies += "org.mockito" % "mockito-core" % "3.4.6" % Test
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "none.controllers._"
 
--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -1,9 +1,17 @@
 package controllers
 
+import model.{GithubUserModel, UserInfoModel, UsersRepoModel}
+import org.mockito.{ArgumentMatcher, ArgumentMatchers}
+import org.scalatestplus.mockito.MockitoSugar
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
+import service.HomeService
+import org.mockito.Mockito._
+import play.api.test.CSRFTokenHelper._
+
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
  * Add your spec here.
@@ -11,35 +19,59 @@ import play.api.test.Helpers._
  *
  * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
  */
-class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with MockitoSugar with Injecting {
+
+
+  private val mockHomeService = mock[HomeService]
+
+  //In unit tests we're not expecting concurrent users, so it's not considered bad to use this here
+  private implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+
+
+  "HomeController POST" should {
+
+    "Render the form page with a users data provided" in {
+      val response = GithubUserModel("jxr227", UsersRepoModel(List.empty, List.empty), UserInfoModel("jxr227", "Telford", "jamesrees94@live.co.uk"))
+
+      when(mockHomeService.getData(ArgumentMatchers.eq("jxr227"))(ArgumentMatchers.any())).thenReturn(Future.successful(response))
+
+      val controller = new HomeController(stubControllerComponents(), mockHomeService)
+      val home = controller.index().apply(FakeRequest(POST, "/github/users").withFormUrlEncodedBody("username" -> "jxr227").withCSRFToken)
+
+      status(home) mustBe OK
+      contentType(home) mustBe Some("text/html")
+      contentAsString(home) must include ("jamesrees94@live.co.uk")
+    }
+
+  }
 
   "HomeController GET" should {
 
-    "render the index page from a new instance of controller" in {
-      val controller = new HomeController(stubControllerComponents())
-      val home = controller.index().apply(FakeRequest(GET, "/"))
+    "render the form page from a new instance of controller" in {
+      val controller = new HomeController(stubControllerComponents(), mockHomeService)
+      val home = controller.formPage().apply(FakeRequest(GET, "/github/users").withCSRFToken)
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Username")
     }
 
-    "render the index page from the application" in {
+    "render the form page page from the application" in {
       val controller = inject[HomeController]
-      val home = controller.index().apply(FakeRequest(GET, "/"))
+      val home = controller.formPage().apply(FakeRequest(GET, "/github/users").withCSRFToken)
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Username")
     }
 
-    "render the index page from the router" in {
-      val request = FakeRequest(GET, "/")
+    "render the form page from the router" in {
+      val request = FakeRequest(GET, "/github/users")
       val home = route(app, request).get
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Username")
     }
   }
 }
